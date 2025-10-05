b9882f789338bd2460397c819a1e8b81f5fa5d77
Amanda Shih <pandafulmanda@gmail.com>
2025-10-05 18:11:32 -0500

    clean up base

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,11 @@
+import type { APIRoute } from "astro";
+
+const getRobotsTxt = (sitemapURL: URL) => `
+User-agent: *
+Disallow: /
+`;
+
+export const GET: APIRoute = ({ site }) => {
+  const sitemapURL = new URL("sitemap-index.xml", site);
+  return new Response(getRobotsTxt(sitemapURL));
+};
