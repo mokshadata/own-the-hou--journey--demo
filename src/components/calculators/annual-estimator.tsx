8d0f12d2fafad498af941f10168e485d1d4f5c74
Amanda Shih <pandafulmanda@gmail.com>
2025-11-17 18:06:12 -0600

    budget calc simple embed

--- src/components/calculators/annual-estimator.tsx
+++ src/components/calculators/annual-estimator.tsx
@@ -7,38 +7,41 @@ export default function AnnualEstimator() {
     }
 
     return (
-        <div class="is-flex is-flex-direction-row">
-            <div class="is-flex is-flex-direction-column">
-                <div>
-                    <input type="text" onChange={handleChange} value={annualIncome()}/>
+        // <Suspense>
+        <div class="workbook--exercise">
+            <div class="is-flex is-flex-direction-row">
+                <div class="is-flex is-flex-direction-column">
+                    <div class="workbook--exercise--explanation">
+                        <p>
+                            <strong>Annual income</strong>: Include the income of everyone who will be involved in making the mortgage payments. This may be just you, or you and a spouse, or you and another family member who will co-sign for your loan.
+                        </p>
+                    </div>
+                    <div class="workbook--exercise--calc">
+                        <input type="text" onChange={handleChange} value={annualIncome()}/>
+                    </div>
                 </div>
-                <div>
-                    <p>
-                        <strong>Annual income</strong>: Include the income of everyone who will be involved in making the mortgage payments. This may be just you, or you and a spouse, or you and another family member who will co-sign for your loan.
-                    </p>
+                <div class="is-flex is-flex-direction-column">
+                    <div class="workbook--exercise--explanation">
+                        <p>
+                            A general rule is that the total price of a home you can afford will be about 2.5 times your annual income.
+                        </p>
+                    </div>
+                    <div class="is-flex is-flex-direction-row workbook--exercise--calc" data-prefix="×">
+                        <input type="text" value="2.5" readonly/>
+                    </div>
                 </div>
             </div>
             <div class="is-flex is-flex-direction-column">
-                <div class="is-flex is-flex-direction-row">
-                    <div>×</div>
-                    <input type="text" value="2.5" />
+                <div class="workbook--exercise--calc workbook--exercise--calc--result">
+                    <input type="text" value={`$${estimate()}`} readonly/>
                 </div>
-                <div>
-                    <p>
-                        A general rule is that the total price of a home you can afford will be about 2.5 times your annual income.
-                    </p>
-                </div>
-            </div>
-            <div class="is-flex is-flex-direction-column">
-                <div>
-                    {estimate()}
-                </div>
-                <div>
+                <div class="workbook--exercise--explanation">
                     <p>
                         This is a quick, <strong>rough estimate</strong> of the price of the home you can afford. When you are ready, you should also get into the details with our full calculator tool, but this will give you an estimate to use for the beginning of your search.
                     </p>
                 </div>
             </div>
         </div>
+        // </Suspense>
     )
 }
--- src/store/navigation.tsx
+++ src/store/navigation.tsx
@@ -1,3 +1,10 @@
 import { createSignal, createMemo, createResource, createEffect} from "solid-js";
+import { makePersisted } from "@solid-primitives/storage";
+import localforage from "localforage";
 
-export const [annualIncome, setAnnualIncome] = createSignal(0)
+export const [annualIncome, setAnnualIncome, init] = makePersisted(createSignal(0), {
+    storage: localforage,
+    name: 'c03.annualIncome',
+})
+
+createResource(() => init)[0]()
