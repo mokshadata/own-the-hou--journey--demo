edd1dc30428427470f4ffe84b713fbb91af07a47
Amanda Shih <pandafulmanda@gmail.com>
2025-12-01 18:48:49 -0600

    video playlist in chapter review

--- src/plugins/remark-register-definitions.mjs
+++ src/plugins/remark-register-definitions.mjs
@@ -1,18 +1,43 @@
 import { visit } from "unist-util-visit";
 
 export function remarkTestArgs() {
-  return function (tree, file) {
-    // visit(tree, function (node) {
-    //   // console.log(node)
-    //   if (node.name === 'VideoPlaceholder') {
-    //     console.log(node.attributes)
-    //     // console.log(node.attributes[0].value.data.estree.body[0].expression)
-    //     // console.log(node.attributes[0].value.data.estree.body[0].expression)
-    //   }
-    // })
-    // console.log(file.data.astro)
-    // console.log(tree)
-    // console.log('tree depth', tree.children[5].children[0].children[0].children[0].children)
-    // console.log('tree dept def', tree.children[5].children[0].children[0].children[1])
+  return function (tree, { data }) {
+    data.astro.frontmatter.videos = []
+
+    const images = tree.children.filter((child) => (child.type === 'mdxjsEsm'))
+      .map((item) => (item.data.estree.body))
+      .reduce((res, curr) => ([...res, ...curr]), [])
+      .filter((item) => (item.specifiers.map((spec) => (spec.local.name)).includes('VideoScreenshot')))
+    
+    const imagesLookup = Object.fromEntries(
+      Object.entries(
+        Object.groupBy(
+          images, (image) => (image.specifiers.map((spec)=>(spec.local.name)).join(''))
+        )
+      ).map(([varName, list]) => ([varName, list[0]]))
+    )
+
+    visit(tree, function (node) {
+      if (node.name === 'VideoPlaceholder') {
+        const imageAttributes = Object.fromEntries(
+          Object.entries(
+            Object.groupBy(node.attributes, (attr) => (attr.name))
+          )
+          .map(([attr, list]) => ([attr, list[0]]))
+          .map(([attr, item]) => ([attr,
+            (attr === 'src' &&
+            {
+              ...item,
+              value: imagesLookup[item.value.value].source.value,
+            }) ||
+            item
+          ]))
+          .map(([attr, item]) => ([attr, item.value]))
+        )
+
+        data.astro.frontmatter.videos.push(imageAttributes)
+      }
+    })
+
   };
 }
