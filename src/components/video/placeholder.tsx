6042f3bd8671c0e81189901359af18f464a5a14a
Amanda Shih <pandafulmanda@gmail.com>
2026-02-14 18:36:44 -0600

    video initial set up

--- src/components/video/placeholder.tsx
+++ src/components/video/placeholder.tsx
@@ -1,7 +1,57 @@
+import ScreenshotMeetYourCounselor from '../../assets/videos/video-9.png'
+import Transcript from '../../assets/videos/video-9.txt'
+
+import { createSignal, createMemo, createResource, createEffect, Show} from "solid-js";
+import { createStore } from "solid-js/store";
+
 export default function VideoPlaceholder({ src, alt, slug }) {
-    return <figure class="video-placeholder home-buyer--cover map--decision" style={{'background-image': `url(${src.src});`}}>
-        <span class="map--decision--cover--background"></span>
+    let videoDOMEl;
+
+    const [hasPlayed, setHasPlayed] = createSignal(false)
+    const [status, setStatus] = createSignal('loading')
+    const [videoLastLoaded, setVideoLastLoaded] = createSignal(0)
+    const [videoDuration, setVideoDuration] = createSignal(0)
+
+    const [videoTime, setVideoTime] = createSignal(0)
+    const [playbackSpeed, setPlaybackSpeed] = createSignal(1)
+    const [transcriptLanguage, setTranscriptLanguage] = createSignal('en-US')
+
+    createEffect(() => {
+        console.log(status())
+        return status()
+    })
+
+    return <figure class={`video-placeholder ${hasPlayed() && 'video--has-played' || 'video--has-not-played'} video--${status()}`}
+    //  style={{'background-image': `url(${src.src});`}}
+     >
+        {/* <span class="map--decision--cover--background"></span> */}
         <figcaption><a name={slug} id={slug}>{alt}</a></figcaption>
-        {/* <img src={src.src} alt={alt} title={alt}/> */}
+        <div class="video--area">
+            <Show when={status() === 'loading'}>
+                <img src={ScreenshotMeetYourCounselor.src} alt={alt} title={alt}/>
+            </Show>
+            <video
+                src="https://ownthehou.s3.us-east-2.amazonaws.com/Video+9+-+Meet+Your+Counselor+-+med.mp4"
+                controls
+                plays-inline
+                ref={(el) => {
+                    videoDOMEl = el;
+                    videoDOMEl.addEventListener("loadeddata", (event) => {
+                        setVideoLastLoaded(event.timeStamp);
+                        setVideoDuration(videoDOMEl.duration);
+
+                        setStatus('loaded')
+                    });
+                    videoDOMEl.addEventListener("timeupdate", (event) => {
+                        setVideoTime(videoDOMEl.currentTime);
+
+                        setHasPlayed(true)
+                        setStatus('playing')
+                    });
+                }}
+            >
+                <track default kind="captions" src={Transcript} srclang="en" />
+            </video>
+        </div>
     </figure>
 }
