40694c60545bfdcfc1e5454f034c515dbc4925cd
Amanda Shih <pandafulmanda@gmail.com>
2026-06-24 13:15:21 -0500

    different intro video for spanish

--- src/components/video/youtube.tsx
+++ src/components/video/youtube.tsx
@@ -3,7 +3,8 @@ import { createSignal } from "solid-js"
 export default function YoutubePlayer(props) {
   const [locale, setLocale] = createSignal(document.documentElement.lang);
   const localedURL = () => (
-    `${props.baseSource.replace('youtube.com', 'youtube-nocookie.com')}&cc_lang_pref=${locale()}-US&hl=${locale()}-US&cc_load_policy=1`
+    props.slug === 'v--introduction-to-the-toolkit' && locale() === 'es' && `https://www.youtube-nocookie.com/embed/2AETizcpxvA?si=DiXFwVeVHJd3i_rq&cc_lang_pref=${locale()}-US&hl=${locale()}-US&cc_load_policy=1&list=PLpK0VohMQcnGvHcg77rylNk0vOfJH7yBv` ||
+    `${props.baseSource.replace('youtube.com', 'youtube-nocookie.com')}&cc_lang_pref=${locale()}-US&hl=${locale()}-US&cc_load_policy=1&list=PLpK0VohMQcnGvHcg77rylNk0vOfJH7yBv`
   )
 
   return (
